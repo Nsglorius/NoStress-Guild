0c74c4d02a40577b77f73de40e263bdcd3746928
Nsglorius <gloriusns@gmail.com>
2015-08-12 14:26:07 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,160 @@
+
+</html>
+
+<footer>
+
+
+<div class="container" style="  background-color: #000000; color: white; ">
+
+<div class="col-lg-4" >
+<fieldset>
+  <legend style=" color: white; ">Recrutement WoW</legend>
+	 	
+		
+		<table>
+<tr>
+       <td>   </td>
+       <td><H4>Classe</H4></td>
+       <td><H4>Besoin</H4></td>
+
+   </tr>
+   <tr>
+	<td><IMG SRC="http://nostressgaming.fr/Image/logo-classe/Chaman.png" ALT="Chaman" TITLE="Chaman" align="center" class="img-thumbnail"></td>
+	<td>Chaman</td>
+	<td>
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/chaman_elemental.png" ALT="Chaman élémenaire" TITLE="Chaman élémenaire" align="center" class="img-thumbnail">-->
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/chaman_enhancement.png" ALT="Chaman amélioration" TITLE="Chaman amélioration" align="center" class="img-thumbnail">-->
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/chaman_restoration.png" ALT="Chaman restauration" TITLE="Chaman restauration" align="center" class="img-thumbnail">-->
+	</td> 
+   </tr>
+   <tr>
+	<td><IMG SRC="http://nostressgaming.fr/Image/logo-classe/Chasseur.png" ALT="Chasseur" TITLE="Chasseur" align="center" class="img-thumbnail"></td>
+	<td>Chasseur</td>
+	<td>
+	<IMG SRC="http://nostressgaming.fr/Image/logo-classe/chasseur_beast_mastery.png" ALT="Chasseur maitrise des bêtes" TITLE="Chasseur maitrise des bêtes" align="center" class="img-thumbnail">
+	<IMG SRC="http://nostressgaming.fr/Image/logo-classe/chasseur_marksmanship.png" ALT="Chasseur précision" TITLE="Chasseur précision" align="center" class="img-thumbnail">
+	<IMG SRC="http://nostressgaming.fr/Image/logo-classe/chasseur_survival.png" ALT="Chasseur survie" TITLE="Chasseur survie" align="center" class="img-thumbnail">
+	</td> 
+   </tr>
+    <tr>
+	<td><IMG SRC="http://nostressgaming.fr/Image/logo-classe/DK.png" ALT="DK" TITLE="DK" align="center" class="img-thumbnail"></td>
+	<td>Chevalier de la mort</td>
+	<td>
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/DK_blood.png" ALT="DK sang" TITLE="DK sang" align="center" class="img-thumbnail">-->
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/DK_unholy.png" ALT="DK impie" TITLE="DK impie" align="center" class="img-thumbnail">-->
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/DK_frost.png" ALT="DK givre" TITLE="DK givre" align="center" class="img-thumbnail">-->
+	</td> 
+   </tr>
+   <tr>
+	<td><IMG SRC="http://nostressgaming.fr/Image/logo-classe/Demoniste.png" ALT="Démoniste" TITLE="Démoniste" align="center" class="img-thumbnail"></td>
+	<td>Démoniste</td>
+	<td>
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/demoniste_demonology.png" ALT="Démoniste démonologie" TITLE="Démoniste démonologie" align="center" class="img-thumbnail">-->
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/demoniste_affliction.png" ALT="Démoniste affliction" TITLE="Démoniste affliction" align="center" class="img-thumbnail">-->
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/demoniste_destruction.png" ALT="Démoniste destruction" TITLE="Démoniste destruction" align="center" class="img-thumbnail">-->
+	</td> 
+	</td> 
+   </tr>
+    <tr>
+	<td><IMG SRC="http://nostressgaming.fr/Image/logo-classe/Druide.png" ALT="Druide" TITLE="Druide" align="center" class="img-thumbnail"></td>
+	<td>Druide</td>
+	<td>
+	<IMG SRC="http://nostressgaming.fr/Image/logo-classe/druide_balance.png" ALT="Druide équilibre" TITLE="Druide équilibre" align="center" class="img-thumbnail">
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/druide_feral.png" ALT="Druide feral" TITLE="Druide feral" align="center" class="img-thumbnail">-->
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/druide_guardian.png" ALT="Druide gardien" TITLE="Druide gardien" align="center" class="img-thumbnail">-->
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/druide_restoration.png" ALT="Druide restauration" TITLE="Druide restauration" align="center" class="img-thumbnail">-->
+	</td> 
+   </tr>
+   <tr>
+	<td><IMG SRC="http://nostressgaming.fr/Image/logo-classe/Guerrier.png" ALT="Guerrier" TITLE="Guerrier" align="center" class="img-thumbnail"></td>
+	<td>Guerrier</td>
+	<td>
+	<IMG SRC="http://nostressgaming.fr/Image/logo-classe/guerrier_arms.png" ALT="Guerrier armes" TITLE="Guerrier armes" align="center" class="img-thumbnail">
+	<IMG SRC="http://nostressgaming.fr/Image/logo-classe/guerrier_fury.png" ALT="Guerrier furie" TITLE="Guerrier furie" align="center" class="img-thumbnail">
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/guerrier_protection.png" ALT="Guerrier protection" TITLE="Guerrier protection" align="center" class="img-thumbnail">-->
+	</td> 
+   </tr>
+   <tr>
+	<td><IMG SRC="http://nostressgaming.fr/Image/logo-classe/Mage.png" ALT="Mage" TITLE="Mage" align="center" class="img-thumbnail"></td>
+	<td>Mage</td>
+	<td>
+	<IMG SRC="http://nostressgaming.fr/Image/logo-classe/mage_arcane.png" ALT="Mage arcane" TITLE="Mage arcane" align="center" class="img-thumbnail">
+	<IMG SRC="http://nostressgaming.fr/Image/logo-classe/mage_fire.png" ALT="Mage feu" TITLE="Mage feu" align="center" class="img-thumbnail">
+	<IMG SRC="http://nostressgaming.fr/Image/logo-classe/mage_frost.png" ALT="Mage givre" TITLE="Mage givre" align="center" class="img-thumbnail">
+	</td> 
+   </tr>
+    <tr>
+	<td><IMG SRC="http://nostressgaming.fr/Image/logo-classe/Moine.png" ALT="Moine" TITLE="Moine" align="center" class="img-thumbnail"></td>
+	<td>Moine</td>
+	<td>
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/moine_brewmaster.png" ALT="Moine maitre brasseur" TITLE="Moine maitre brasseur" align="center" class="img-thumbnail">-->
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/moine_mistweaver.png" ALT="Moine tissebrume " TITLE="Moine tissebrume" align="center" class="img-thumbnail">
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/moine_windwalker.png" ALT="Moine marchevent" TITLE="Moine marchevent" align="center" class="img-thumbnail">-->
+	</td> 
+   </tr>
+   <tr>
+	<td><IMG SRC="http://nostressgaming.fr/Image/logo-classe/Paladin.png" ALT="Paladin" TITLE="Paladin" align="center" class="img-thumbnail"></td>
+	<td>Paladin</td>
+	<td>
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/paladin_holy.png" ALT="Paladin sacré" TITLE="Paladin sacré" align="center" class="img-thumbnail">-->
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/paladin_protection.png" ALT="Paladin protection" TITLE="Paladin protection" align="center" class="img-thumbnail">-->
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/paladin_retribution.png" ALT="Paladin vindicte" TITLE="Paladin vindicte" align="center" class="img-thumbnail">-->
+	</td> 
+   </tr>
+    <tr>
+	<td><IMG SRC="http://nostressgaming.fr/Image/logo-classe/Pretre.png" ALT="Pretre" TITLE="Pretre" align="center" class="img-thumbnail"></td>
+	<td>Prêtre</td>
+	<td>
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/pretre_discipline.png" ALT="Prêtre discipline" TITLE="Prêtre discipline" align="center" class="img-thumbnail">-->
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/pretre_holy.png" ALT="Prêtre sacré" TITLE="Prêtre sacré" align="center" class="img-thumbnail">-->
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/pretre_shadow.png" ALT="Prêtre ombre" TITLE="Prêtre ombre" align="center" class="img-thumbnail">-->
+	</td> 
+   </tr>
+   <tr>
+	<td><IMG SRC="http://nostressgaming.fr/Image/logo-classe/Voleur.png" ALT="Voleur" TITLE="Voleur" align="center" class="img-thumbnail"></td>
+	<td>Voleur</td>
+	<td>
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/voleur_assassination.png" ALT="Voleur assasinat" TITLE="Voleur assasinat" align="center" class="img-thumbnail">-->
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/voleur_combat.png" ALT="Voleur combat" TITLE="Voleur combat" align="center" class="img-thumbnail">-->
+	<!--<IMG SRC="http://nostressgaming.fr/Image/logo-classe/voleur_subtlety.png" ALT="Voleur finesse" TITLE="Voleur finesse" align="center" class="img-thumbnail">-->
+	</td> 
+   </tr>
+    
+</table>
+</fieldset>	 
+</div>
+<div class="col-lg-4">
+<fieldset>
+  <legend style=" color: white; ">Lien utile</legend>
+		<a href="http://www.wowprogress.com/" style=" color: white";>WoW progress</a>	</br>
+		<a href="http://www.icy-veins.com/" style=" color: white;"> Icy veins</a>	</br>
+		<a href="http://eu.battle.net/wow/fr/" style=" color: white;"> Battle.net</a>	</br>
+		<a href="https://www.facebook.com/nostressgaming.fr" style=" color: white;">NoStress Facebook</a>	</br>
+		<a href="http://www.heroesfire.com/" style=" color: white;">Heroes fire</a>	</br>
+	 
+
+</fieldset> 
+</div>
+<div class="col-lg-4">
+<fieldset>
+  <legend style=" color: white; " >Log</legend>
+
+</fieldset>
+
+</div>
+</div>
+
+
+</footer>
+
+
+<!-- /.container -->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="http://nostressgaming.fr/jquery.js"></script>
+    <script src="http://nostressgaming.fr/bootstrap/js/bootstrap.min.js"></script>
+
+<?php
+mysql_close();
+?> 
